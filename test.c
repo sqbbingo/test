0e47ceb85b4970845c6e0ce9db1e5eb08ee18327
bingo <2577200835@qq.com>
2024-07-15 23:43:35 +0800

    add test.c

---
+++ test.c
@@ -0,0 +1,13 @@
+/*
+* @Author: 25772
+* @Date:   2024-07-15 23:36:57
+* @Last Modified by:   25772
+* @Last Modified time: 2024-07-15 23:37:17
+*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("test\n");
+    return 0;
+}
