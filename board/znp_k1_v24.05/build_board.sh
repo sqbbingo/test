9cdc7bb15afefdd648e496d94f997a44600de444
Su Qibin <qibin.su@zf.com>
2024-07-25 11:01:44 +0800

    fix wifi

--- board/znp_k1_v24.05/build_board.sh
+++ board/znp_k1_v24.05/build_board.sh
@@ -7,14 +7,12 @@ function board_pull_source {
 
 #将配置和patch应用到源码
 function board_patch {
-    pwd
     rsync -av --progress board/$BOARD  build/
-    ls build/config/boards
 }
 
 #编译
 function board_armbian_compile {
     cd build/
-    ./compile.sh build BOARD=makego-ZNP BRANCH=edge BUILD_DESKTOP=no BUILD_MINIMAL=yes KERNEL_CONFIGURE=no RELEASE=bookworm
+    ./compile.sh build BOARD=makego BRANCH=edge BUILD_DESKTOP=no BUILD_MINIMAL=yes KERNEL_CONFIGURE=no RELEASE=bookworm
     cd -
 }
